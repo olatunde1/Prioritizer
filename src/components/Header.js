cc57c70cd434d6faf2e51a4209c8ccbb47a8b537
Olatunde <idowuolatunde32@gmail.com>
2022-07-28 02:46:10 +0100

    project completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ const Header = ({handleToggleDarkMode}) => {
     return (
         <div className="header">
              {/*the heading of our notes app*/}
-        <h1><span style={{color:"#308d46"}}>Your</span> Prioritizer</h1>
+        <h1><span style={{color:"red"}}>Your</span> Prioritizer</h1>
          {/*this is our toggle button 
         using this button we can change the background theme for our application*/}
         <button onClick={()=>handleToggleDarkMode((previousDarkMode)=>!previousDarkMode)} className="save">Toggle Mode</button>
+
         </div>
+        
     )
 }
 
